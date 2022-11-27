4e4fa383c5e0743da541ae05036af2c059773f9a
TrQuan173 <trquan173@gmail.com>
2022-11-27 16:12:55 +0700

    Quan config and build the basic structure

---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+// Layout
+
+// Page
+import Product from '../pages/Product'
+import Order from '../pages/Order'
+import Profile from '../pages/Profile'
+
+const publicRoutes = [
+]
+
+const privateRoutes = [
+    { path: '/', component: Product},
+    { path: '/order', component: Order},
+    { path: '/profile', component: Profile}
+]
+
+export { publicRoutes, privateRoutes}
